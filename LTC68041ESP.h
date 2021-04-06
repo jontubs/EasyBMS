16dc95b20b7aeeb25c7e8d170ac2c50ab44ac59a
derBastler <mail100100101@gmail.com>
2021-04-06 21:58:08 +0200

    added Header description

--- LTC68041ESP.h
+++ LTC68041ESP.h
@@ -1,51 +1,11 @@
-/*!
-LTC6804-1 Multicell Battery Monitor
+/************************************************************
 
+This library is based on the LTC68041.cpp by linear technology.
 http://www.linear.com/product/LTC6804-1
 
-http://www.linear.com/product/LTC6804-1#demoboards
+I modified it make it compatible with the ESP8622
 
-
-Copyright 2018(c) Analog Devices, Inc.
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
- - Redistributions of source code must retain the above copyright
-   notice, this list of conditions and the following disclaimer.
- - Redistributions in binary form must reproduce the above copyright
-   notice, this list of conditions and the following disclaimer in
-   the documentation and/or other materials provided with the
-   distribution.
- - Neither the name of Analog Devices, Inc. nor the names of its
-   contributors may be used to endorse or promote products derived
-   from this software without specific prior written permission.
- - The use of this software may or may not infringe the patent rights
-   of one or more patent holders.  This license does not release you
-   from the requirement that you obtain separate licenses from these
-   patent holders to use this software.
- - Use of the software either in source or binary form, must be run
-   on or directly connected to an Analog Devices Inc. component.
-
-THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
-MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Copyright 2013 Linear Technology Corp. (LTC)
 ***********************************************************/
-
-/*! @file
-    @ingroup LTC68041
-    Header for LTC6804-1 Multicell Battery Monitor
-*/
 
 #ifndef LTC68041_H
 #define LTC68041_H
@@ -147,6 +107,11 @@ static const unsigned int crc15Table[256] = {0x0,0xc599, 0xceab, 0xb32, 0xd8cf, 
 #define CELL_CH_5and11 5
 #define CELL_CH_6and12 6
 
+
+static const byte csPin = D8;
+static const byte pinMOSI = D7;
+static const byte pinMISO = D6;
+static const byte pinCLK = D5;
 
 /*!
 
