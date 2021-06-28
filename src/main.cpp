906abaa6e2642ac3fee4ac6cf7333b9e4fa5d740
derBastler <mail100100101@gmail.com>
2021-06-28 22:36:21 +0200

    running version without wifi for debugging

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ int error = 0;
 //byte pinMOSI, byte pinMISO, byte pinCLK, byte csPin
 static LTC68041 LTC = LTC68041(D7, D6, D5, D8);
 
+static unsigned long timer;
+static unsigned long diff=1000;
+static bool flip;
+
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -24,12 +28,8 @@ void setup()
 // the loop function runs over and over again forever
 void loop()
 {
-  //Set some config values
-  LTC.CFGRw[0]=0xFE;   //0x07;     //0xFE;
-  LTC.CFGRw[5]=0;
-  LTC.setVUV(3.1);    //undervoltage detection
-  LTC.setVOV(3.9);    //overvoltage detection
 
+  
   if (LTC.checkSPI_mute())
   {
     digitalWrite(D1, HIGH);
@@ -40,12 +40,46 @@ void loop()
     digitalWrite(D1, LOW);
     Serial.println("\nSPI lost");
   }
+  //Set the CFGWrite bytes
+  LTC.CFGRw[0]=0xFE;   //0x07;     //0xFE;
+
+  LTC.CFGRw[5]=0;
+  LTC.setVUV(3.1);
+  LTC.setVOV(3.9);
+
+  //Local Variables
+  if((timer+diff)<millis())
+  {
+    timer=millis();
+    flip= !flip;
+  }
+
+  if (flip)
+  {
+
+    digitalWrite(D2, HIGH);
+    LTC.CFGRw[4]=0;
+    LTC.CFGRw[5]=0;
+    //LTC.DischargeW[0]=1;
+    //LTC.DischargeW[8]=1;
+  }
+  else
+  {
+    //einfach ein bisschen die discharge pins flippen um zu gucken obs geht
+    digitalWrite(D2, LOW);
+    LTC.CFGRw[4]=0xFF;
+    LTC.CFGRw[5]=0x0F;
+    //LTC.DischargeW[0]=0;
+    //LTC.DischargeW[8]=0;
+  }
+    
+  LTC.wrcfg(LTC.CFGRw);
   //Start different Analog-Digital-Conversions in the Chip
+  
   LTC.adcv();
   LTC.adax();
   LTC.adstat();
-
-  delay(20); //Wait until everything is finished
+  delay(20);   //Wait until everything is finished
 
   //Read the raw values into the controller
   LTC.rdcv();
@@ -53,34 +87,30 @@ void loop()
   LTC.rdauxb();
   LTC.rdstata();
   LTC.rdstatb();
+  LTC.rdcfg();
+  
   //Convert the raw values into clear text values
   LTC.cnvCellVolt();
   LTC.cnvAuxVolt();
   LTC.cnvStatus();
-
-  Serial.print("\nIC Temperature: ");
-  Serial.print(LTC.InternalTemp);
+  //LTC.cnvDischarge(LTC.DischargeW);
+  //LTC.rditemp();
+  //Print the clear text values cellVoltage, gpioVoltage, Undervoltage Bits, Overvoltage Bits
+  
+  Serial.print("\nConfig zurückgelesen: ");
+  printArrayByte(6, LTC.CFGRr);
   Serial.print("\r\n");
-  /*
+
   Serial.print("\nCell Voltages: ");
-  printArrayFloat(LTC.cellNum, LTC.cellVoltage);
+  for(int i=0;i<LTC.cellNum;i++)
+  {
+    Serial.print(LTC.cellVoltage[i]);
+    Serial.print("\t");
+  } 
+
+  Serial.print("\nModule Voltage: ");
+  Serial.print(LTC.SumCellVoltages);
   Serial.print("\r\n");
 
-  Serial.print("\nGPIO Voltages: ");
-  printArrayFloat(LTC.gpioNum, LTC.gpioVoltage);
-  Serial.print("\r\n");
-
-  Serial.print("\n Raw Status Values: ");
-  printArrayByte(LTC.SizeReg, LTC.STAR);
-  Serial.print("\r\n");
-
-  Serial.print("\n Cell Undervoltage detected: ");
-  printArrayBool(LTC.cellNum, LTC.CUV);
-  Serial.print("\r\n");
-
-  Serial.print("\n Cell Overvoltage detected:  ");
-  printArrayBool(LTC.cellNum, LTC.COV);
-  Serial.print("\r\n");
-  */
-  delay(200);
+  delay(100);
 }
