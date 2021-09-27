edfa5932bd815ccfa6c5b7f1ee63913ed93259d8
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-09-27 17:33:05 +0200

    added cell balancing

--- src/main.cpp
+++ src/main.cpp
@@ -6,33 +6,30 @@
 
 #include "credentials.h"
 
-int error = 0;
 static LTC68041 LTC = LTC68041(D8);
 
 static const long master_timeout = 5000;
 
-unsigned int module_index = 3;
-String hostname = String("esp-module/") + module_index;
+unsigned int module_index = 24;
+String hostname = String("esp-module-") + module_index;
+String module_topic = String("esp-module/") + module_index;
 
 WiFiClient espClient;
 PubSubClient client(mqtt_server, 1883, espClient);
 
-unsigned long uptime_millis;
-// std::array<float, 12> cell_voltage;
+std::array<unsigned long, 12> cells_to_balance{};
 
 // connect to wifi
-void connectWifi()
-{
+void connectWifi() {
     Serial.println();
     Serial.print("connecting to ");
     Serial.println(ssid);
-    WiFi.persistent(false);
+    ESP8266WiFiClass::persistent(false);
     WiFi.softAPdisconnect(true);
     WiFi.mode(WIFI_STA);
     WiFi.hostname(hostname);
     WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
+    while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
     }
@@ -47,26 +44,22 @@ void connectWifi()
     Serial.println(IPAddress(dns_getserver(1)));
 }
 
-void reconnect()
-{
+void reconnect() {
     // Loop until we're reconnected
-    while (!client.connected())
-    {
+    while (!client.connected()) {
         Serial.print("Attempting MQTT connection...");
         // Attempt to connect
-        if (client.connect(WiFi.macAddress().c_str(), mqtt_username, mqtt_password, (hostname + "/available").c_str(), 0, true, "offline"))
-        {
+        if (client.connect(hostname.c_str(), mqtt_username, mqtt_password, (module_topic + "/available").c_str(), 0,
+                           true, "offline")) {
             Serial.println("connected");
             // Once connected, publish an announcement...
-            client.publish((hostname + "/available").c_str(), "online", true);
+            client.publish((module_topic + "/available").c_str(), "online", true);
             // ... and resubscribe
-            client.subscribe((hostname + "/cell_discharge").c_str());
+            client.subscribe((module_topic + "/cell_discharge").c_str());
             client.subscribe(String("master/uptime").c_str());
-            client.subscribe((hostname + "/cell_overvoltage_limit").c_str());
-            client.subscribe((hostname + "/cell_undervoltage_limit").c_str());
-        }
-        else
-        {
+            client.subscribe((module_topic + "/cell_overvoltage_limit").c_str());
+            client.subscribe((module_topic + "/cell_undervoltage_limit").c_str());
+        } else {
             Serial.print("failed, rc=");
             Serial.print(client.state());
             //   Serial.println(" try again in 5 seconds");
@@ -77,17 +70,20 @@ void reconnect()
     }
 }
 
+String payload_to_string(byte *payload, unsigned int length) {
+    String result = "";
+    for (unsigned int i = 0; i < length; i++) {
+        result += static_cast<char>(payload[i]);
+    }
+    return result;
+}
+
 long last_master_uptime = 0;
 
-void callback(char *topic, byte *payload, unsigned int length)
-{
-    if (!strcmp(topic, String("master/uptime").c_str()))
-    {
-        String uptime = "";
-        for (unsigned int i = 0; i < length; i++)
-        {
-            uptime += static_cast<char>(payload[i]);
-        }
+void callback(char *topic, byte *payload, unsigned int length) {
+    String topic_string = String(topic);
+    if (topic_string.equals("master/uptime")) {
+        String uptime = payload_to_string(payload, length);
         Serial.print("Got heartbeat from master: ");
         Serial.println(uptime);
 
@@ -97,54 +93,24 @@ void callback(char *topic, byte *payload, unsigned int length)
 
         long uptime_long = uptime.toInt();
 
-        if (uptime_long - last_master_uptime > master_timeout)
-        {
+        if (uptime_long - last_master_uptime > master_timeout) {
             Serial.println(">>> Master Timeout!!");
         }
-    }
-    else if (!strcmp(topic, (hostname + "/cell_discharge").c_str()))
-    {
-        String cell_discharge = "";
-        for (unsigned int i = 0; i < length; i++)
-        {
-            cell_discharge += static_cast<char>(payload[i]);
+    } else if (topic_string.startsWith(module_topic)) {
+        if (topic_string.startsWith(module_topic + "/cell/")) {
+            String get_cell = topic_string.substring((module_topic + "/cell/").length());
+            get_cell = get_cell.substring(0, get_cell.indexOf("/"));
+            long cell_number = get_cell.toInt();
+            if (topic_string.equals(module_topic + "/cell/" + cell_number + +"/balance/set")) {
+                long balance_time = payload_to_string(payload, length).toInt();
+                cells_to_balance.at(cell_number - 1) = millis() + balance_time;
+            }
         }
-        Serial.println(cell_discharge);
-        // bool cell_discharge = payload[0];
-        // Serial.println(cell_discharge);
-    }
-    else if (!strcmp(topic, (hostname + "/alive_counter_master").c_str()))
-    {
-        String alive_counter_master = "";
-        for (unsigned int i = 0; i < length; i++)
-        {
-            alive_counter_master += static_cast<char>(payload[i]);
-        }
-        Serial.println(alive_counter_master);
-    }
-    else if (!strcmp(topic, (hostname + "/cell_overvoltage_limit").c_str()))
-    {
-        String alive_counter_master = "";
-        for (unsigned int i = 0; i < length; i++)
-        {
-            alive_counter_master += static_cast<char>(payload[i]);
-        }
-        Serial.println(alive_counter_master);
-    }
-    else if (!strcmp(topic, (hostname + "/cell_undervoltage_limit").c_str()))
-    {
-        String alive_counter_master = "";
-        for (unsigned int i = 0; i < length; i++)
-        {
-            alive_counter_master += static_cast<char>(payload[i]);
-        }
-        Serial.println(alive_counter_master);
     }
 }
 
 // the setup function runs once when you press reset or power the board
-void setup()
-{
+[[maybe_unused]] void setup() {
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(D5, OUTPUT);
@@ -158,24 +124,18 @@ void setup()
 
     LTC.initSPI(D7, D6, D5); //Initialize LTC6804 hardware
 
-    // for (int i = 0; i < 12; i++)
-    // {
-    //     cell_voltage.at(i) = 2.8 + (0.1 * i);
-    // }
     client.setCallback(callback);
 }
 
 // the loop function runs over and over again forever
-void loop()
-{
-    if (LTC.checkSPI(true))
-    {
+void loop() {
+    unsigned long uptime_millis = millis();
+
+    if (LTC.checkSPI(true)) {
         digitalWrite(D1, HIGH);
         Serial.println();
         Serial.println("SPI ok");
-    }
-    else
-    {
+    } else {
         digitalWrite(D1, LOW);
         Serial.println();
         Serial.println("SPI lost");
@@ -184,12 +144,18 @@ void loop()
     LTC.cfgSetVUV(3.1);
     LTC.cfgSetVOV(4.2);
 
-    // digitalWrite(D2, HIGH);
-    // LTC.cfgSetDCC(std::bitset<12>{0b000000000000});  //alternative bitset.reset()
-
-    // //einfach ein bisschen die discharge pins flippen um zu gucken obs geht
-    // digitalWrite(D2, LOW);
-    // LTC.cfgSetDCC(std::bitset<12>{0b111111111111});  //alternative bitset.set()
+    std::bitset<12> balance_bits{};
+    for (size_t i = 0; i < cells_to_balance.size(); ++i) {
+        if (cells_to_balance[i] >= uptime_millis) {
+            balance_bits[i] = true;
+        }
+    }
+    if (balance_bits.any()) {
+        digitalWrite(D2, HIGH);
+    } else {
+        digitalWrite(D2, LOW);
+    }
+    LTC.cfgSetDCC(balance_bits);
 
     LTC.cfgWrite();
     //Start different Analog-Digital-Conversions in the Chip
@@ -202,8 +168,9 @@ void loop()
     delay(5); //Wait until conversion is finished
 
     //Read the raw values into the controller
-    std::array<float, 6> voltages;
-    LTC.getCellVoltages(voltages, LTC68041::CH_ALL); // read all channel (2nd parameter default), use only 6 (size of array)
+    std::array<float, 6> voltages{};
+    LTC.getCellVoltages(voltages,
+                        LTC68041::CH_ALL); // read all channel (2nd parameter default), use only 6 (size of array)
     LTC.getAuxVoltage(LTC68041::CHG_ALL);            // nothing is read, just for documenation of usage
     LTC.getStatusVoltage(LTC68041::CHST_ALL);        // nothing is read, just for documenation of usage
     LTC.cfgRead();
@@ -214,29 +181,31 @@ void loop()
     LTC.readAuxDbg();
     LTC.readCellsDbg();
 
-    std::array<float, 12> cell_voltages;
+    std::array<float, 12> cell_voltages{};
     LTC.getCellVoltages(cell_voltages);
     float chip_temp = LTC.getStatusVoltage(LTC.StatusGroup::CHST_ITMP);
 
     float module_voltage = LTC.getStatusVoltage(LTC68041::CHST_SOC);
 
-    delay(500);
-    if (!client.connected())
-    {
+    if (!client.connected()) {
         reconnect();
     }
     client.loop();
 
-    uptime_millis = millis();
-    client.publish((hostname + "/uptime").c_str(), String(uptime_millis).c_str(), true);
+    client.publish((module_topic + "/uptime").c_str(), String(uptime_millis).c_str(), true);
 
-    for (size_t i = 0; i < cell_voltages.size(); i++)
-    {
-        client.publish((hostname + "/cell/" + String(i + 1) + "/voltage").c_str(), String(cell_voltages[i]).c_str(), true);
+    for (size_t i = 0; i < cell_voltages.size(); i++) {
+        client.publish((module_topic + "/cell/" + String(i + 1) + "/voltage").c_str(), String(cell_voltages[i]).c_str(),
+                       true);
+        if (balance_bits.test(i)) {
+            client.publish((module_topic + "/cell/" + String(i + 1) + "/balance").c_str(), "balancing", true);
+        } else {
+            client.publish((module_topic + "/cell/" + String(i + 1) + "/balance").c_str(), "stop", true);
+        }
     }
-    client.publish((hostname + "/module_voltage").c_str(), String(module_voltage).c_str(), true);
-    client.publish((hostname + "/module_temps").c_str(), (String(25.0) + "," + String(25.1)).c_str(), true);
-    client.publish((hostname + "/chip_temp").c_str(), String(chip_temp).c_str(), true);
+    client.publish((module_topic + "/module_voltage").c_str(), String(module_voltage).c_str(), true);
+    client.publish((module_topic + "/module_temps").c_str(), (String(25.0) + "," + String(25.1)).c_str(), true);
+    client.publish((module_topic + "/chip_temp").c_str(), String(chip_temp).c_str(), true);
 
     delay(1000);
 }
